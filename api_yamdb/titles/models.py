91346bf1a390f24f674cfc00c1af9ced49b68acb
Бабийчук Богдан <babiychuk.bogdan@mail.ru>
2023-03-29 15:38:11 +0200

    models\

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -4,24 +4,48 @@ from django.db import models
 User = get_user_model()
 
 
+class Category(models.Model):
+    """Класс Категорий"""
+    name = models.CharField(max_length=100,
+                            verbose_name='Название Категории')
+    slug = models.SlugField(verbose_name='Slug',
+                            unique=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    """Модель Жанров"""
+    name = models.CharField(
+        max_length=75,
+        verbose_name='Название жанра',
+        db_index=True
+    )
+    slug = models.SlugField(
+        verbose_name='Slug',
+        unique=True)
+    
+    def __str__(self):
+        return self.name
+
+
 class Title(models.Model):
+    """Модель произведений"""
+
     name = models.CharField(max_length=256)
-    year = models.IntegerField()
-    description = models.TextField()
-    # genre = models.ForeignKey(
-    #     Genre, on_delete=models.SET_NULL,
-    #     related_name='genre_posts', blank=True, null=True
-    # )
-    # category = models.ForeignKey(
-    #     Category, on_delete=models.SET_NULL,
-    #     related_name='category_posts', blank=True, null=True
-    # )
+    year = models.IntegerField(verbose_name='Год написания')
+    description = models.TextField(blank=True)
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL,related_name='genre_posts', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,related_name='category_posts', blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Review(models.Model):
+    """Модель отзывов"""
+
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='reviews'
     )
@@ -33,45 +57,3 @@ class Review(models.Model):
         'Дата добавления', auto_now_add=True, db_index=True
     )
     score = models.IntegerField()
-
-
-# class Group(models.Model):
-#     title = models.CharField(max_length=200)
-#     slug = models.SlugField(unique=True)
-#     description = models.TextField()
-
-#     def __str__(self):
-#         return self.title
-
-
-# class Post(models.Model):
-#     text = models.TextField()
-#     pub_date = models.DateTimeField(
-#         'Дата публикации', auto_now_add=True
-#     )
-#     author = models.ForeignKey(
-#         User, on_delete=models.CASCADE, related_name='posts'
-#     )
-#     image = models.ImageField(
-#         upload_to='posts/', null=True, blank=True
-#     )  # поле для картинки
-#     group = models.ForeignKey(
-#         Group, on_delete=models.SET_NULL,
-#         related_name='posts', blank=True, null=True
-#     )
-
-#     def __str__(self):
-#         return self.text
-
-
-# class Comment(models.Model):
-#     author = models.ForeignKey(
-#         User, on_delete=models.CASCADE, related_name='comments'
-#     )
-#     post = models.ForeignKey(
-#         Post, on_delete=models.CASCADE, related_name='comments'
-#     )
-#     text = models.TextField()
-#     created = models.DateTimeField(
-#         'Дата добавления', auto_now_add=True, db_index=True
-#     )

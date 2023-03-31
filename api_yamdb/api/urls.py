68f689fa3d98c124d64791fd2947d4c63d5092d7
galeks <jsgroup_mail@ngs.ru>
2023-03-31 14:53:30 +0700

    sprint10 yamdb 20230331 comments by galeks

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import TitleViewSet, ReviewViewSet
+from api.views import TitleViewSet, ReviewViewSet, CommentViewSet
 
 
 router_v1 = routers.DefaultRouter()
@@ -10,6 +10,11 @@ router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
     basename='reviews'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
 )
 
 urlpatterns = [
